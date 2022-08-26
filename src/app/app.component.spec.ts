2545a6988d55fa28bd5e230a3c928605db11956a
marodseg <manucazorla9@gmail.com>
2022-08-26 22:19:33 +0200

    Test prettier functionality

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -24,7 +24,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('confianza-animal');
-        // testing prettier
   });
 
   it('should render title', () => {
