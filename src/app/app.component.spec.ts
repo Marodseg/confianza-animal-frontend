701b59bc48a6d82aa37ed095db2e606d64810601
marodseg <manucazorla9@gmail.com>
2022-08-26 22:10:22 +0200

    Add lint, prettier and husky configuration

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -17,7 +17,7 @@ describe('AppComponent', () => {
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app).toBeFalse();
+    expect(app).toBeTruthy();
   });
 
   it(`should have as title 'confianza-animal'`, () => {
